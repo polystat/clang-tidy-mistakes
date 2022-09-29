0175b46526c794ae0442b877e04f5f012e940498
andrewt0301 <andrewt0301@gmail.com>
2022-09-29 10:27:25 +0300

    recursion is updated
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- false-negative/infinite-recursion.cpp
+++ false-negative/infinite-recursion.cpp
@@ -1,10 +1,10 @@
-// Clang-Tidy cannot detect infinite recursion.
+// Clang-Tidy cannot detect infinite recursion even for trivial cases.
 
 #include <iostream>
 
 // This function contains infinite recursion.
 int recursion(int x) {
-    return recursion(x + 1) + recursion(x - 1);
+    return recursion(x + 1);
 }
 int main() {
     std::cout << recursion(10) << std::endl;
