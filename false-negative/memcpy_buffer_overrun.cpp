55d5bee1549e869bb9f94d528fb2182f15469e41
andrewt0301 <andrewt0301@gmail.com>
2022-10-10 14:03:09 +0300

    Added example of buffer overrun 'memcpy'
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

---
+++ false-negative/memcpy_buffer_overrun.cpp
@@ -0,0 +1,24 @@
+// Clang-Tidy fails to detect writing beyond the scope of an array with memcpy.
+
+#include <cstring>
+
+const int src_arr[] = { 5, 6, 7, 8};
+
+// Need this function to hide memcpy. Otherwise, error is detected.
+void write(void * dest, const void * src, const size_t size) {
+    memcpy(dest, src, size);
+}
+
+int main() {
+    int x = 1;
+    int dest_arr[] = {1, 2, 3, 4};
+    while (true) {
+        if (x == 0) {
+            return 0;
+        }
+        x = 0;
+        // This "write" corrupts the x variable causing endless loop.
+        write(dest_arr + 3, src_arr, sizeof(src_arr));
+    }
+    return 0;
+}
