c84f10bbb20107542197468921a82525a0a44284
Yegor Bugayenko <yegor256@gmail.com>
2022-10-13 10:47:18 +0300

    simplified

--- false-negative/buffer-overrun.cpp
+++ false-negative/buffer-overrun.cpp
@@ -13,14 +13,10 @@ int find_index(const int* arr, const int count, const int val) {
 }
 
 // This needs to be in a separate function (to get the symptoms of stack corruption).
-void test() {
+int main() {
     int arr[] = {1, 2, 5, 6};
     // Buffer overrun is here: arr[N] is assigned corrupting the stack.
     // Array index depends on function return value.
     arr[find_index(arr, 4, 3)] = 10;
-}
-
-int main() {
-    test();
     return 0;
 }
