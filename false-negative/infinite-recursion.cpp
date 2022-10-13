6a9c1cf8a36f1768d43363778dc907d9444ebdf7
Yegor Bugayenko <yegor256@gmail.com>
2022-10-13 09:59:26 +0300

    shorter

--- false-negative/infinite-recursion.cpp
+++ false-negative/infinite-recursion.cpp
@@ -1,12 +1,8 @@
 // Clang-Tidy cannot detect infinite recursion even for trivial cases.
-
-#include <iostream>
-
 // This function contains infinite recursion.
-int recursion(int x) {
-    return recursion(x + 1);
+int f(int x) {
+  return f(x + 1);
 }
 int main() {
-    std::cout << recursion(10) << std::endl;
-    return 0;
+  return f(10);
 }
